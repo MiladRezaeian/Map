780c29693e9523d523ca37573ea13160e83c24d3
Milad Rezaeian <milad.rezaeiann@gmail.com>
2022-09-04 16:58:53 +0430

    add leaflet map

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>7Map</title>
+    <link href="favicon.png" rel="shortcut icon" type="image/png">
+    <link rel="stylesheet" href="assets/css/leaflet.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+    <script src="assets/js/leaflet.js"></script>
+</head>
+
+<body>
+<div class="main">
+    <div class="head">
+        <input type="text" id="search" placeholder="دنبال کجا می گردی؟">
+    </div>
+    <div class="mapContainer">
+        <div id="map"></div>
+    </div>
+</div>
+
+<script>
+
+    var map = L.map('map').setView([35.7074926, 51.2075678], 11);
+
+    var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+</script>
+
+</body>
+
+</html>
